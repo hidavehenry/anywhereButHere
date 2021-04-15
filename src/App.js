060bad46d5dd4e1b4e7b6a1fafdf510bff4e8c4c
StephenSchooley <schooley.sa@gmail.com>
2021-04-15 16:28:11 -0400

    whaddup Mapples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Input from './components/Input'
+import axios from 'axios'
 
 function App() {
+
+  /* 
+  Create component to take user input
+  User submits location or chooses use my location
+  User selects what to search for
+  User searches, on search updates state to equal value of filled in feilds
+  Makes a call to search API using the two locations
+  Create component to take names of returned results objects
+  Map over returned array for name property
+  Pass data to list component
+  User selects name, updates thir state
+  API call to direction API
+  Create component for return
+  */
+
+  // API CALL FOR SEARCH
+  axios( {
+    method:'GET',
+    url: 'https://www.mapquestapi.com/search/v4/place',
+    responseType: 'json',
+      params: {
+        location: [-79.41753141902342, 43.649976291114896],
+        sort: 'distance',
+        key: '70bf0d125d0d890aaada4fc8639854d0',
+        feedback: false,
+        circle: [-79.41753141902342, 43.649976291114896, 5000],
+        q: `coffee`
+      }
+    }).then(function (res) {
+      console.log(res);
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Input />
     </div>
   );
 }
