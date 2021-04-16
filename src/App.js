26c09b65b724a0b7ab9d71809a105a8797ac94fa
ajamalirad <aplasticlanguage@gmail.com>
2021-04-16 17:57:57 -0400

    stf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Input from './components/Input'
-import axios from 'axios'
+// import axios from 'axios'
 
 function App() {
 
@@ -19,21 +19,37 @@ function App() {
   */
 
   // API CALL FOR SEARCH
-  axios( {
-    method:'GET',
-    url: 'https://www.mapquestapi.com/search/v4/place',
-    responseType: 'json',
-      params: {
-        location: [-79.41753141902342, 43.649976291114896],
-        sort: 'distance',
-        key: '70bf0d125d0d890aaada4fc8639854d0',
-        feedback: false,
-        circle: [-79.41753141902342, 43.649976291114896, 5000],
-        q: `coffee`
-      }
-    }).then(function (res) {
-      console.log(res);
-    });
+  // axios( {
+  //   method:'GET',
+  //   url: 'https://www.mapquestapi.com/search/v4/place',
+  //   responseType: 'json',
+  //     params: {
+  //       location: [-79.41753141902342, 43.649976291114896],
+  //       sort: 'distance',
+  //       key: '70bf0d125d0d890aaada4fc8639854d0',
+  //       feedback: false,
+  //       circle: [-79.41753141902342, 43.649976291114896, 5000],
+  //       q: `coffee`
+  //     }
+  //   }).then(function (res) {
+  //     console.log(res);
+  //   });
+
+  const proxiedUrl = 'https://www.mapquestapi.com/search/v4/place';
+const url = new URL('https://proxy.hackeryou.com');
+url.search = new URLSearchParams({
+  reqUrl: proxiedUrl,
+  'params[location]': [-79.41753141902342, 43.649976291114896],
+  'params[sort]': 'distance',
+  'params[key]': 'GvTYDdAzlzCU5UcQ00cnarwGMaBtz8gi',
+  'params[circle]': [-79.41753141902342, 43.649976291114896, 5000],
+  'params[q]': 'coffee',
+});
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  });
 
   return (
     <div>
